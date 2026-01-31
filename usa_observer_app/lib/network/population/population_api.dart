e041aabd405010a9295f0fe74fb16d5d7c29cd31
Anton Skidan <antonskwork@gmail.com>
2026-01-31 16:37:58 +0300

    remove dependency

--- usa_observer_app/lib/network/population/population_api.dart
+++ usa_observer_app/lib/network/population/population_api.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:usa_observer_app/network/population/models/models.dart';
 
 class PopulationApi {
   final http.Client _client;
